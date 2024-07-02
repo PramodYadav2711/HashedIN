d9d4deafb20638ed834642c99c43c7f17dbfac65
gahlot1999 <gahlot1999@gmail.com>
2024-07-02 22:20:32 +0530

    shape added

--- vite-project/src/Sections/Fold3.jsx
+++ vite-project/src/Sections/Fold3.jsx
@@ -1,20 +1,19 @@
 function Fold3() {
   return (
-    <div className="bg-white ">
-      <div className="flex max-w-[80vw] mx-auto items-center justify-center">
-        <div className="flex flex-col lg:flex-row ">
-          {/* Text Section */}
-          <div className="lg:w-1/2 p-10 flex flex-col justify-center">
-            <h2 className="text-2xl text-[#2c2f55] mb-4">
+    <div className='bg-white relative overflow-hidden'>
+      <div className='flex max-w-[80vw] mx-auto items-center justify-center'>
+        <div className='flex flex-col lg:flex-row '>
+          <div className='lg:w-1/2 p-10 flex flex-col justify-center'>
+            <h2 className='text-2xl text-[#2c2f55] mb-4'>
               Joint surgeon, Dr. John Hahn believes he has discovered the
               secret. And he's got
-              <span className="font-bold ">
+              <span className='font-bold '>
                 {' '}
                 hundreds of thousands of followers
               </span>
               , some even in their nineties, to prove it.
             </h2>
-            <p className="text-gray-600 mb-4">
+            <p className='text-gray-600 mb-4'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lorem
               sem, mollis sed viverra id, cursus at dolor. Maecenas ac semper
               sem, in lobortis est. Cras pharetra vitae tellus quis commodo.
@@ -23,22 +22,15 @@ function Fold3() {
               facilisis at urna. Cras a vulputate risus. Nam venenatis enim et
               libero molestie tincidunt.
             </p>
-            <p className="text-gray-600 mb-4">
+            <p className='text-gray-600 mb-4'>
               nunc leo, eget suscipit ex semper quis. Quisque mollis ornare
               consectetur. Cras facilisis sem erat, et facilisis ante dignissim
-              in. Suspendisse ex libero, pellentesque ac tincidunt id, posuere
-              vitae lorem. Aenean vitae tortor augue. Maecenas convallis sodales
-              nulla vitae auctor. Mauris tincidunt auctor ante.
-            </p>
-            <p className="text-gray-600">
-              nunc leo, eget suscipit ex semper quis. Quisque mollis ornare
-              consectetur. Cras facilisis sem erat, et facilisis ante dignissim
-              in. Suspendisse ex libero,
+              in. Suspendisse ex libero.
             </p>
           </div>
 
           {/* Image Section */}
-          <div className="lg:w-1/2 relative flex items-center justify-center overflow-hidden">
+          {/* <div className="lg:w-1/2 relative flex items-center justify-center overflow-hidden">
             <div className="absolute inset-0 bg-green-500 rounded-xl transform -skew-y-6"></div>
             <img
               src="src/assets/doctor.png"
@@ -55,7 +47,8 @@ function Fold3() {
               <p className="font-semibold">Dr. John Hahn</p>
               <p>DPM, ND, America’s Joint Doctor</p>
             </div>
-          </div>
+          </div> */}
+          <div className='shape h-96 w-[50vw] bg-green-400 rounded-[3.5rem] skew-y-6 absolute -right-40 top-20'></div>
         </div>
       </div>
     </div>
